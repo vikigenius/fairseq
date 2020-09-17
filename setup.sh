f4a993b880b0a1b94169c079389aadb6eb8e63f9
vikigenius <v9balasu@uwaterloo.ca>
2020-09-17 14:11:06 -0400

    Added deactivate and pyarrow to setup

--- setup.sh
+++ setup.sh
@@ -2,11 +2,13 @@
 
 (git clone https://github.com/NVIDIA/apex && wget git https://dl.fbaipublicfiles.com/fairseq/models/bart.large.tar.gz) &
 conda create -n fairseq python=3 -y
-source activate fairseq
+conda activate fairseq
 conda install pytorch -c pytorch -y
 wait
 cd apex
 pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" \
   --global-option="--deprecated_fused_adam" --global-option="--xentropy" \
   --global-option="--fast_multihead_attn" ./
+pip install pyarrow
+conda deactivate
 cd ..
